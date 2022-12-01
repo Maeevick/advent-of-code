652b2f627a35e60b920617b82ff026295f739895
Aurel <sayag_vs@hotmail.fr>
2022-12-01 22:45:43 +0100

    day 1

--- node/src/day_01/solution.test.ts
+++ node/src/day_01/solution.test.ts
@@ -1,7 +1,20 @@
-import { main } from "./solution"
+import { readInputSync } from "../readInputSync"
+import { daily, first, second } from "./solution"
 
 describe('when day 1 opens', () => {
     test('then I start to code', () => {
-        expect(main()).toEqual('day 1 !')
+        expect(daily()).toEqual('day 1 !')
+    })
+})
+
+describe('when the elves enter in the jungle', () => {
+    test('then the elf carrying the most, carries (in calories)', () => {
+        expect(first(readInputSync(`${__dirname}/sample.txt`))).toEqual(24000)
+    })
+})
+
+describe('when the elves enter in the jungle', () => {
+    test('then the 3 elves carrying the most, carries a total of (in calories)', () => {
+        expect(second(readInputSync(`${__dirname}/sample.txt`))).toEqual(45000)
     })
 })
--- node/src/day_01/solution.ts
+++ node/src/day_01/solution.ts
@@ -1,1 +1,23 @@
-export const main = () => 'day 1 !'
+import { readInputSync } from "../readInputSync"
+
+export const daily = () => 'day 1 !'
+
+const getCaloriesPerElf = (data: string) => {
+    return data
+        .split('\n\n')
+        .map(i => {
+            return i.split('\n')
+                .map(Number)
+                .reduce((a, c) => c + a, 0)
+        })
+}
+
+export const first = (data: string) => {
+    return Math.max(...getCaloriesPerElf(data))
+}
+console.log(daily(), 'first answer is :', first(readInputSync(`${__dirname}/data.txt`)))
+
+export const second = (data: string) => {
+    return getCaloriesPerElf(data).sort((a, b) => b - a).slice(0,3).reduce((a, c) => c + a, 0)
+}
+console.log(daily(), 'second answer is :', second(readInputSync(`${__dirname}/data.txt`)))
