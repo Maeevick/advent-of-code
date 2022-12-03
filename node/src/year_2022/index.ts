f25b81ac190098513603d9999fa694e9646d7f44
Aurel <sayag_vs@hotmail.fr>
2022-12-03 07:57:37 +0100

    prepare day 4

--- node/src/year_2022/index.ts
+++ node/src/year_2022/index.ts
@@ -3,6 +3,7 @@ import { showAnswersForYearAndDay } from '../logAnswersForDay'
 import { daily as day01, first as first01, second as second01 } from './day_01/solution'
 import { daily as day02, first as first02, second as second02 } from './day_02/solution'
 import { daily as day03, first as first03, second as second03 } from './day_03/solution'
+import { daily as day04, first as first04, second as second04 } from './day_04/solution'
 
 const SEPARATOR = '-'.repeat(32)
 const YEAR = 'year_2022'
@@ -16,3 +17,5 @@ console.log(SEPARATOR)
 showAnswersForYearAndDay(YEAR, day02(), first02, second02)
 console.log(SEPARATOR)
 showAnswersForYearAndDay(YEAR, day03(), first03, second03)
+console.log(SEPARATOR)
+showAnswersForYearAndDay(YEAR, day04(), first04, second04)
