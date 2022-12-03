a2047a79356ae975394dc6697bfcf4c1900285e6
Aurel <sayag_vs@hotmail.fr>
2022-12-03 07:55:29 +0100

    day 3

--- node/src/year_2022/day_03/solution.test.ts
+++ node/src/year_2022/day_03/solution.test.ts
@@ -2,20 +2,20 @@ import path from 'node:path'
 import { readInputSync } from '../../readInputSync'
 import { daily, first, second } from './solution'
 
-describe('when ...', () => {
-    test('then ...', () => {
+describe('when day 3 opens', () => {
+    test('then I start to code', () => {
         expect(daily()).toEqual('day_03')
     })
 })
 
-describe('when ... for the first time', () => {
-    test('then ...', () => {
-        expect(first(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(0)
+describe('when elves check priorities of items presents in each ruckracks for the first time', () => {
+    test('then the sum of the priorities is', () => {
+        expect(first(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(157)
     })
 })
 
-describe('when ... for the second time', () => {
-    test('then ...', () => {
-        expect(second(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(0)
+describe('when elves check the badges presents in each ruckracks for the second time', () => {
+    test('then the sum of badges priorities is', () => {
+        expect(second(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(70)
     })
 })
--- node/src/year_2022/day_03/solution.ts
+++ node/src/year_2022/day_03/solution.ts
@@ -1,9 +1,69 @@
 export const daily = () => 'day_03'
 
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+const UPPER_ALPHABET = ALPHABET.toUpperCase()
+
+const SCORE_MAP = ALPHABET.split('')
+    .reduce((a: Record<string, number>, c: string, i: number) => {
+        a[c] = i + 1
+        a[c.toUpperCase()] = i + 27
+
+        return a
+    }, {})
+
+const getScore = (char: string) => {
+    return SCORE_MAP[char]
+}
+
 export const first = (data: string) => {
-    return 0
+    return sum(
+        computePrioritiesScore(
+            getRuckracks(data)
+        )
+    )
 }
 
 export const second = (data: string) => {
-    return 0
+    return computeBagdesScore(
+        getRuckracks(data)
+    )
+}
+
+function getRuckracks(data: string) {
+    return data.split('\n')
+}
+
+function computePrioritiesScore(ruckracks: string[]) {
+    return ruckracks.map(backpack => {
+        const pivot = backpack.length / 2
+        const [first, second] = [backpack.slice(0, pivot).split(''), backpack.slice(pivot).split('')]
+        while (first.length > 0) {
+            const head = first.shift() as string
+            if (second.includes(head)) {
+                return getScore(head)
+            }
+
+        }
+        return 0
+    })
+}
+
+function sum(values: number[]) {
+    return values.reduce((a, c) => c + a, 0)
+}
+
+function computeBagdesScore(ruckracks: string[]) {
+    let index = 0
+    let score = 0
+    while (index < ruckracks.length) {
+        const [fst, snd, trd] = ruckracks.slice(index, index + 3)
+        for (const c of `${ALPHABET}${UPPER_ALPHABET}`) {
+            if (fst.includes(c) && snd.includes(c) && trd.includes(c)) {
+                score += getScore(c)
+                break
+            }
+        }
+        index += 3
+    }
+    return score
 }
