b808dbe9a1817344d89a486fab6baebc1112b14c
Aurel <sayag_vs@hotmail.fr>
2022-12-10 07:58:46 +0100

    day10

--- node/src/year_2022/day_10/solution.test.ts
+++ node/src/year_2022/day_10/solution.test.ts
@@ -8,14 +8,21 @@ describe('when day 10 opens', () => {
     })
 })
 
-describe('when ... for the first time', () => {
-    test('then ...', () => {
-        expect(first(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(0)
+describe('when the signal strenght is mesured after 220 cycles for the first time', () => {
+    test('then the sum of signal streght is', () => {
+        expect(first(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(13140)
     })
 })
 
-describe('when ... for the second time', () => {
-    test('then ...', () => {
-        expect(second(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(0)
+describe('when CRT draws pixel following CPU instructions for the second time', () => {
+    test('then the final screen is', () => {
+        expect(second(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(
+`##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....`
+)
     })
 })
--- node/src/year_2022/day_10/solution.ts
+++ node/src/year_2022/day_10/solution.ts
@@ -1,9 +1,70 @@
 export const daily = () => 'day_10'
 
+const ADDX = 'addx'
+const CRT_SIZE = 240
+
 export const first = (data: string) => {
-    return 0
+    const registerValues = generateAllRegisterValues(data)
+    let sum = 0
+    for (const i in registerValues) {
+        const cycle = Number(i) + 1
+        if (isMeasureCycle(cycle)) {
+            sum += computeRegisterValuesAtIndex(registerValues, cycle) * cycle
+        }
+    }
+    return sum
 }
 
 export const second = (data: string) => {
-    return 0
+    const registerValues = generateAllRegisterValues(data)
+    let pixels = ''
+    for (const i in registerValues) {
+        const cycle = Number(i) + 1
+        if (cycle <= CRT_SIZE) {
+            const crtIndex = computeCRTIndex(Number(i))
+            const xRegister = computeRegisterValuesAtIndex(registerValues, cycle)
+
+            pixels += appendPixelSymbolCharacter(xRegister, crtIndex)
+            pixels += appendEndOfLineCharacterWhenNecessary(cycle)
+        }
+    }
+    return pixels
 }
+
+function generateAllRegisterValues(data: string) {
+    const registerValues = [1]
+
+    const instructions = data.split('\n').map(i => i.split(' '))
+    for (const [instruction, value] of instructions) {
+        registerValues.push(0)
+        if (instruction === ADDX) {
+            registerValues.push(Number(value))
+        }
+    }
+    return registerValues
+}
+
+function isMeasureCycle(cycle: number) {
+    return cycle === 20 || cycle % 40 === 20
+}
+
+function computeRegisterValuesAtIndex(registerValues: number[], i: number) {
+    return registerValues.slice(0, i).reduce((a, c) => a + c)
+}
+
+function computeCRTIndex(i: number) {
+    return isEndOfCRTLine(i + 1) ? 39 : i % 40
+}
+
+function isEndOfCRTLine(i: number) {
+    return i % 40 === 0
+}
+
+function appendPixelSymbolCharacter(xRegister: number, crtIndex: number) {
+    return Math.abs(xRegister - crtIndex) > 1 ? '.' : '#'
+}
+
+function appendEndOfLineCharacterWhenNecessary(cycle: number) {
+    return cycle !== CRT_SIZE && isEndOfCRTLine(cycle) ? '\n' : ''
+}
+
