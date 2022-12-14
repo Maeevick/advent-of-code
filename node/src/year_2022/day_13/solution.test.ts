dfdbfb77969f67eb02a15cf04cba852f7d72c5a2
Aurel <sayag_vs@hotmail.fr>
2022-12-14 23:01:49 +0100

    day 13

--- node/src/year_2022/day_13/solution.test.ts
+++ node/src/year_2022/day_13/solution.test.ts
@@ -8,14 +8,14 @@ describe('when day 13 opens', () => {
     })
 })
 
-describe('when ... for the first time', () => {
-    test('then ...', () => {
-        expect(first(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(0)
+describe('when comparing distress signal packets for the first time', () => {
+    test('then the sum of indices of packets in the right order is', () => {
+        expect(first(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(13)
     })
 })
 
-describe('when ... for the second time', () => {
-    test('then ...', () => {
-        expect(second(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(0)
+describe('when sorting packets in the right order for the second time', () => {
+    test('then the product of divider packets indices is', () => {
+        expect(second(readInputSync(path.join(__dirname, 'sample.txt')))).toEqual(140)
     })
 })
--- node/src/year_2022/day_13/solution.ts
+++ node/src/year_2022/day_13/solution.ts
@@ -1,9 +1,49 @@
 export const daily = () => 'day_13'
 
+const UNKNOWN = 0
+const RIGHT = -1
+const WRONG = 1
+const DIVIDER_PACKETS = ['[[2]]', '[[6]]']
+
 export const first = (data: string) => {
-    return 0
+    return data.split('\n\n')
+        .map(p => p.split('\n'))
+        .map(([fst, snd]) => checkPairsOfIndex(fst, snd))
+        .reduce((a, c, i) => c < 0 ? a + i + 1 : a, 0)
 }
 
 export const second = (data: string) => {
-    return 0
+
+    const packets = data.split('\n\n')
+        .map(p => p.split('\n'))
+        .flatMap((p) => p)
+        .concat(...DIVIDER_PACKETS)
+        .sort((fst, snd) => checkPairsOfIndex(fst, snd))
+
+    return packets.reduce((a,c,i) => DIVIDER_PACKETS.includes(c) ? a * (i + 1) : a, 1)
+}
+function checkPairsOfIndex(first: string, second: string): number {
+    const firstE = eval(first)
+    const secondE = eval(second)
+
+    if (first == undefined && second == undefined) return UNKNOWN
+    if (first == undefined) return RIGHT
+    if (second == undefined) return WRONG
+
+    if (areNumbers(firstE, secondE) && firstE === secondE) return UNKNOWN
+    if (areNumbers(firstE, secondE) && firstE < secondE) return RIGHT
+    if (areNumbers(firstE, secondE) && firstE > secondE) return WRONG
+
+    const f = Array.isArray(firstE) ? firstE : [firstE]
+    const s = Array.isArray(secondE) ? secondE : [secondE]
+
+    for (let i = 0, d = Math.max(f.length, s.length); i < d; i++) {
+        const r = checkPairsOfIndex(f[i], s[i])
+        if (r !== UNKNOWN) return r
+    }
+    return UNKNOWN
+}
+
+function areNumbers(a: any, b: any) {
+    return typeof a == 'number' && typeof b == 'number'
 }
